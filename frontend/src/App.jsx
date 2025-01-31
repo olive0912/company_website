b7e8157463e5b832ad31b3cbce2b86f230e7530c
olive0912 <3san0912@gmail.com>
2025-01-31 13:14:54 +0900

    04_Hero페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,62 @@ import './App.css'
 import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import MainPage from './Page/MainPage/MainPage';
+import About from './Page/About/About';
+import Leadership from './Page/Leadership/Leadership';
+import Board from './Page/Board/Board';
+import Services from './Page/Services/Services';
+import Contact from './Page/Contact/Contact';
 
+function Layout() {//1
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
+const router = createBrowserRouter([//2
+  {
+    path: '/', //최초접속
+    element: <Layout />,
+    children: [ //중첩 라우터
+      {
+        index: true,
+        element: <MainPage />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/leadership',
+        element: <Leadership />
+      },
+      {
+        path: '/board',
+        element: <Board />
+      },
+      {
+        path: '/our-services',
+        element: <Services />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
 
-function App() {
+    ]
+  }
+]);
+
+function App() {//3
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-    
+    <RouterProvider router={router} />
   );
 }
 
